734820de46f5dd60230d9692422991deb8c67643
Brian Omondi <brianhilsden@gmail.com>
2024-03-21 14:18:48 +0300

    used find method to get year team won

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // code your solution here
+function superbowlWin(record){
+    const winDetails=record.find(item=>item["result"]==="W")
+    if(winDetails){
+        return winDetails["year"]
+    }
+    else{
+        return undefined
+    }
+    
+}
